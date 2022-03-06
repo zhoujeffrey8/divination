b60a687987ca9376455143dea3a5d2e3d30f08e4
Addison <addisonghoff@gmail.com>
2022-03-06 12:27:39 -0700

    openaiapi

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,31 @@ const router = express().use(bodyParser.json())
 router.post('/', async (req, res) => {
     // do something
     console.log(req.body)
-    url = ""
-    settings = {}
+    // url = ""
+    // settings = {}
     // const resp = await fetch(url, settings)
     // url and settings are both variables
     //       .then(response => response.json())
 
     // resp is the object that is returned
     // return it as a response
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion("text-davinci-001", {
+    prompt: "",
+    temperature: 0.7,
+    max_tokens: 64,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    });
+
+
     return res.json({newString: "YAY"})
 })
 
